da525ce1ea8b4be9832963aa3acb86510b959a0c
Trevor Westphal <trevorawestphal@gmail.com>
2017-03-04 08:59:32 -0800

    Added bone axe prefab, setup fx script, almost ready to throw it

---
+++ Assets/Scripts/EffectsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsController : MonoBehaviour {
+    // Just a macro to despawn an object when it's out of frame
+    private float radius = 0.1f;
+
+	void Update(){
+        Vector3 screenposition = Camera.main.WorldToViewportPoint(transform.position);
+
+        if(1.0f < screenposition.y - radius || 0.0f > screenposition.y + radius ||
+           1.0f < screenposition.x - radius || 0.0f > screenposition.x + radius){
+               Destroy(gameObject);
+        }
+	}
+}
