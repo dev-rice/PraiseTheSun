854045834a8b9aad4cfead72963cbda5d736c758
Trevor Westphal <trevorawestphal@gmail.com>
2017-03-06 22:32:42 -0800

    Added ground trap, cleaned up enemy prefabs

---
+++ Assets/Scripts/GroundTrapController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTrapController : Movable {
+    public Sprite closedSprite;
+    public GameObject weaponInstance;
+    private GameObject trappedObject;
+
+    private bool triggered; // REEEEEEEEEEE
+    public float trappedTime;
+    private float currentTime;
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy"){
+            // set sprite
+            GetComponent<SpriteRenderer>().sprite = closedSprite;
+
+            // create weapon
+            GameObject weapon = Instantiate(weaponInstance);
+            weapon.transform.position = transform.position;
+
+            triggered = true;
+            trappedObject = other.gameObject;
+        }
+    }
+
+    void Update(){
+        if(!triggered){
+            return;
+        }
+
+        currentTime += Time.deltaTime;
+
+        if(currentTime > trappedTime){
+            Destroy(gameObject);
+        }
+    }
+
+    void LateUpdate(){
+        if(triggered){
+            trappedObject.transform.position = transform.position + new Vector3(0.5f, 0.5f, 0.0f);
+        }
+    }
+}
--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(BoxCollider2D))]
 public class WeaponDamage : MonoBehaviour {
     public bool destroyOnImpact;
     public int damage;
+
+    void Start(){
+        tag = "Weapon"; // Just in case
+    }
 }
