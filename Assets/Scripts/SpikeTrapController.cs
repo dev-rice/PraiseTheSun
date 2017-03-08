919638a4f789f67f3febeaf0d6fee8c2da2c7055
Trevor Westphal <trevorawestphal@gmail.com>
2017-03-07 18:00:57 -0800

    SPike trap half-working

---
+++ Assets/Scripts/SpikeTrapController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrapController : Movable {
+
+    public Sprite bloodiedSprite;
+    public int damage;
+    private bool triggered;
+
+    private GameObject target;
+    private float targetx;
+
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.gameObject.tag == "Player" || collider.gameObject.tag == "Enemy"){
+            if(collider.gameObject.GetComponent<Rigidbody2D>().velocity.y < 0.0f){
+                triggered = true;
+                GetComponent<SpriteRenderer>().sprite = bloodiedSprite;
+                target = collider.gameObject;
+                targetx = collider.gameObject.transform.position.x;
+            }
+        }
+    }
+
+    void LateUpdate(){
+        if(triggered){
+            target.transform.position = new Vector3(targetx, target.transform.position.y, 0.0f);
+        }
+    }
+}
