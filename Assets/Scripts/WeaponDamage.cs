9eaa9a61546012b59abfe1511323f1ff372cd5c7
Trevor Westphal <trevorawestphal@gmail.com>
2017-03-08 22:44:09 -0800

    Fixed all the fuckups from previous commit - weapons vanishing instantly

--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent (typeof(BoxCollider2D))]
 public class WeaponDamage : MonoBehaviour {
     public bool destroyOnImpact;
+    public bool destroyAfterTime;
     public int damage;
     private int frames = 15;
 
@@ -13,10 +14,12 @@ public class WeaponDamage : MonoBehaviour {
     }
 
     void Update(){
-        frames--;
+        if(destroyAfterTime){
+            frames--;
 
-        if(frames == 0){
-            Destroy(gameObject);
+            if(frames == 0){
+                Destroy(gameObject);
+            }
         }
     }
 }
