22282d9d96e3979a6f55ea4991be4f4898e2029c
Trevor Westphal <trevorawestphal@gmail.com>
2017-03-09 19:11:40 -0800

    Gorillaz

---
+++ Assets/Scripts/GorillaController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GorillaController : Movable {
+    // Gorillas simply idle until player gets close enough
+    // then leap towards them attacking
+
+    // State handling
+    public enum GorillaState {
+        Idle,
+        Crouching,
+        Jumping,
+    }
+
+    public enum GorillaDirection {
+        Right,
+        Left
+    }
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if(IsGrounded()){
+            // standin anim
+        } else {
+            // anim to jumping
+        }
+	}
+}
